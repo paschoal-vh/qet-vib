43fac739e82164eed2ede7411fe2ca8647ace8b2
paschoal-vh <paschoal.vhp@usp.br>
2021-05-07 11:31:32 -0300

    changes in qet_vib.py and __init__.py

--- qet_vib/__init__.py
+++ qet_vib/__init__.py
@@ -4,9 +4,10 @@ QET-Vib: A Quantum Espresso Toolbox for Vibrational Spectroscopy
 """
 
 # Add imports here
-from .data_management import *
-from .eigenvector_tools import *
-from .raman_tools import *
+from .data_management import atomic_mass, extract
+from .eigenvector_tools import fdv
+from .raman_tools import raman_gen, raman_calc
+from .born_tools import born_gen, born_calc
 
 # Handle versioneer
 from ._version import get_versions
--- qet_vib/qet_vib.py
+++ qet_vib/qet_vib.py
@@ -5,7 +5,26 @@ QET-Vib: A Quantum Espresso Toolbox for Vibrational Spectroscopy
 Handles the primary functions
 """
 
+import sys, argparse, numpy
+from .data_management import atomic_mass, extract
+from .eigenvector_tools import fdv
+from .raman_tools import raman_gen, raman_calc
+from .born_tools import born_gen, born_calc
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='QET-Vib: A Quantum Espresso Toolbox for Vibrational Spectroscopy')
+
+    parser.add_argument('--extract', description='Call extraction module for q=(0 0 0) eigenvectors')
+    parser.add_argument('--fdv', description='Calculate mode participation share of translations, librations and intramolecular motions')
+    parser.add_argument('--raman_gen', description='Generate Raman inputs for calculations with the finite difference method')
+    parser.add_argument('--raman_calc', description='Calculate Raman intensities for calculations with the finite difference method')
+    parser.add_argument('--born_gen', description='Generate Born charge tensor and dielectric constant imputs for calculations with the finite difference method')
+
+    
+
+    args=parser.parse_args()
+    
 
 if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print('Hellow World!')
+    main()
