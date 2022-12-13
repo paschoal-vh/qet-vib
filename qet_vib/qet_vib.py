3e1066d6d647cab5fa0404200d17a0450e92386c
paschoal-vh <64969691+paschoal-vh@users.noreply.github.com>
2022-12-13 15:49:51 -0300

    Update qet_vib.py
    Corrected action in add_argument

--- qet_vib/qet_vib.py
+++ qet_vib/qet_vib.py
@@ -17,23 +17,23 @@ def main():
         description='QET-Vib: '\
         'A Quantum Espresso Toolbox for Vibrational Spectroscopy')
 
-    parser.add_argument('--extract',nargs='append',
+    parser.add_argument('--extract',action='append',
                         help='Call extraction module for q=(0 0 0) eigenvectors.'\
                         'Arguments are filename with the eigenvectors (defaults'\
                         'to eigen.out). The output directory (defaults to '\
                         './test_output) And ph.x output (defaults to ph.out)'\
                         'e.g.: --extract eigen.out;test_output;ph.out')
-    parser.add_argument('--fdv',nargs='append',
+    parser.add_argument('--fdv',action='append',
                         help='Calculate mode participation of translations, '\
                         'librations and intramolecular motions. Needs as '\
                         'argument the mode number, the  system field from pw.x,'\
                         'read from pw.in (default) and the output directory '\
                         'used with --extract. '\
                         'e.g. --fdv 1;test_output --pwin pw.in')
-    parser.add_argument('--pwin', default='pw.in',
+    parser.add_argument('--pwin', action='pw.in',
                         help='Name of the pw.x input file used in for the ph.x '\
                         'calculations, defaults to pw.in')
-    parser.add_argument('--raman_gen',nargs='append',
+    parser.add_argument('--raman_gen',action='append',
                         help='Generate Raman inputs for calculations with DFPT+'\
                         'Finite difference approach. Expects as arguments mode'\
                         'number, the directory from --extract, other parameters'\
@@ -41,12 +41,12 @@ def main():
                         'deformation done to the reference structure in the'\
                         'direction of the eigenvector.'\
                         'e.g. --raman_gen 1;0.1 --pwin pw.in')
-    parser.add_argument('--raman_calc',nargs='append',
+    parser.add_argument('--raman_calc',action='append',
                         help='Calculate Raman intensities from DFPT+finite '\
                         'difference method. Takes as arguments mode number and'\
                         'the name of the ph.x output (which defaults to ph.out)'\
                         'e.g. --raman_calc 1;ph.out')
-#    parser.add_argument('--born_gen',nargs='append',
+#    parser.add_argument('--born_gen',action='append',
 #                        help='Generate Born charge tensor and dielectric constant'\
 #                        'inputs for calculations with the finite difference method.')
     args=parser.parse_args()
